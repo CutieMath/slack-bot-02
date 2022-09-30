a692403a7a97b03fbb4ef678febaebc42a26b6de
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-30 12:11:41 +1000

    send message to channel

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import { RTMClient } from "@slack/rtm-api";
+import { WebClient } from "@slack/web-api";
 
 const rtm = new RTMClient(process.env.SLACK_TOKEN);
+const web = new WebClient(process.env.SLACK_TOKEN);
 rtm.start().catch(console.error);
 
 rtm.on("ready", async () => {
   console.log("Girl I'm LIT");
+  sendMessage("#general", "Girl I'm online ye");
 });
+
+async function sendMessage(channel, message) {
+  await web.chat.postMessage({
+    channel: channel,
+    text: message,
+  });
+}
