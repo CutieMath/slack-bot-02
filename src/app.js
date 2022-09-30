91499fa845b15de1d01736d4259ca425dd1c0ae9
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-30 15:33:26 +1000

    find user name and send private message to user

--- src/app.js
+++ src/app.js
@@ -17,20 +17,67 @@ async function createChannelAndSendMessage() {
   await publishMessage(
     conversationOpenResult,
     conversationOpenResult.channel.id,
-    "Test message"
+    usersStore["U044RJDU7DX"]
   );
 }
 createChannelAndSendMessage();
 
-async function publishMessage(results, id, text) {
-  console.log("CHANNEL CREATION RESULTS: ", results);
+async function publishMessage(results, id, userName) {
+  console.log("CONVERSATION OPEN RESULTS: ", results);
   console.log("CHANNEL ID RECEIVED: ", id);
+  console.log("USER NAME RECEIVED: ", userName);
   try {
     // Call the chat.postMessage method using the built-in WebClient
     const result = await web.chat.postMessage({
       token: process.env.SLACK_TOKEN,
       channel: id,
-      text: text,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hello, *${userName}*, your direct nudge is here!`,
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: " \n\n *Nudge:* \n\nA bunch of texts. ",
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Yes I do",
+                emoji: true,
+              },
+              value: "click_me_123",
+              url: "https://google.com",
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Skip for now",
+                emoji: true,
+              },
+              value: "click_me_123",
+              url: "https://google.com",
+            },
+          ],
+        },
+      ],
     });
     console.log(result);
   } catch (error) {
@@ -39,10 +86,11 @@ async function publishMessage(results, id, text) {
 }
 
 const saveUsers = (usersArray) => {
-  console.log("USERS: ", usersArray);
+  // console.log("USERS: ", usersArray);
   let userId = "";
   usersArray.forEach((user) => {
     userId = user.id;
-    usersStore[userId] = user;
+    usersStore[userId] = user.name;
   });
+  console.log("USERS STORE: ", usersStore);
 };
